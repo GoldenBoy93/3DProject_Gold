f1203307f8b9e7172f2ba5437302d20834e60461
minjeparkk <zone8572@gmail.com>
2025-08-11 14:09:58 +0900

    Feat : 2초후 발사 구현

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Canon : MonoBehaviour
@@ -9,7 +11,15 @@ public class Canon : MonoBehaviour
         // 충돌한 콜라이더의 리지드바디를 변수로 가져옴
         Rigidbody Rb = other.GetComponent<Rigidbody>();
 
+        StartCoroutine(Shoot(Rb));
+    }
+
+    IEnumerator Shoot(Rigidbody Rb)
+    {
+        // 00초 대기
+        yield return new WaitForSeconds(2f);
+
         // 리지드바디에 에드포스
-        Rb.AddForce(new Vector3(1,1,0) * shootPower, ForceMode.Impulse);
+        Rb.AddForce(new Vector3(1, 1, 0) * shootPower, ForceMode.Impulse);
     }
 }
